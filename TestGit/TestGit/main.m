7c136727d578284c42f02293ab6630600cc6547b
TRSherly <827660214@qq.com>
2015-07-01 11:31:02 +0800

    test sending code to remote.

---
+++ TestGit/TestGit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
